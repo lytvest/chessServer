f7f06b50681aaef59af007c79db48641a35cda94
Дмитрий Башкирцев <bdm1999@yandex.ru>
2022-01-17 22:22:16 +0500

    Initial commit

---
+++ src/main/java/ru/lytvest/chessserver/Answer.java
@@ -0,0 +1,14 @@
+package ru.lytvest.chessserver;
+
+public class Answer {
+
+    public String name = "NoName";
+
+    Answer(){
+
+    }
+
+    Answer(String name){
+        this.name = name;
+    }
+}
---
+++ src/test/java/ru/lytvest/chessserver/ChessserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lytvest.chessserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
